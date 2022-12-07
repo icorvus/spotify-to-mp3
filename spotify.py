8b4c87a4d3d903c8232b05d76649a382e8f0cd9c
icorvus <business.wrona@gmail.com>
2022-12-07 15:47:31 +0100

    Added url cleaner/handler

--- spotify.py
+++ spotify.py
@@ -2,6 +2,20 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
+import re
+
+
+def handle_url(url: str) -> dict:
+    url_pattern = r"spotify.com\/(track|playlist|album)\/([A-Za-z0-9]+)"
+    match = re.search(url_pattern, url)
+    if match:
+        return {
+            "type": match[1],
+            "id": match[2]
+        }
+    return {
+        "type": "invalid"
+    }
 
 
 # Load environment variables from .env file
@@ -11,3 +25,6 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 api_credentials = SpotifyClientCredentials(client_id=CLIENT_ID,
                                            client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=api_credentials)
+
+while True:
+    handle_url(input("URL: "))
